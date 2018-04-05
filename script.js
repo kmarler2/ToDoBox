05bfad85467168e084250617dae660b13db2dc4f
Brian Weikel <brianweikel@Brians-MacBook-Pro.local>
2018-04-05 09:46:24 -0600

    style CSS, responsiveness and beautify

--- script.js
+++ script.js
@@ -1,40 +1,35 @@
-
 var titleInput = document.querySelector('.input');
 var bodyInput = document.querySelector('#body');
 var saveButton = document.querySelector('.save');
-// var thingsToShow = document.querySelector('.container-bottom');
-var deleteButton = document.querySelector('.delete-button');
+var deleteButton = document.querySelector('.delete-button-header');
 var quality = document.querySelector('.quality');
 var ideaQuality = 0;
 var qualityDisplay = ['quality: swill', 'quality: plausible', 'quality: genius'];
 var key;
 
 saveButton.addEventListener('click', saveInput);
-$('.container-bottom').on('click', '.delete-button', deleteCard);
+$('.container-bottom').on('click', '.delete-button-header', deleteCard);
 $('.container-bottom').on('click', '.upvote', upvoteChange);
 $('.container-bottom').on('click', '.downvote', downVoteChange);
 $(window).on('load', restoreCard);
 
-
-
 function saveInput(e) {
- e.preventDefault();
- var title = titleInput.value;
- var body = bodyInput.value;
- var newCard = new MakeCard(title, body);
- prependCard(newCard);
- clearInput();
- storeCard(newCard);
+  e.preventDefault();
+  var title = titleInput.value;
+  var body = bodyInput.value;
+  var newCard = new MakeCard(title, body);
+  prependCard(newCard);
+  clearInput();
+  storeCard(newCard);
 }
 
 function prependCard(object) {
   var ideaCard = document.createElement('article');
   ideaCard.innerHTML = `
   <article class="cards" id=${object.id}>
-     <h4 class="header" contenteditable="true">${object.title}
-      <img class="delete-button header" id="delete" src="FEE-ideabox-icon-assets/delete.svg" alt="">
-     </h4>
-
+     <span class="idea-head">
+      <img class="delete-button-header" id="delete" src="FEE-ideabox-icon-assets/delete.svg" alt="">
+      <h4 class="header" contenteditable="true">${object.title}</h4></span>
      <p class="description" contenteditable="true">${object.body}</p>
      <div class="quality-line">
      <img class="upvote" src="FEE-ideabox-icon-assets/upvote.svg" alt="" role="upvote button">
@@ -53,21 +48,19 @@ function MakeCard(title, body, id, ideaQuality) {
   this.ideaQuality = ideaQuality || 0;
 }
 
-function storeCard(newCard) {  
+function storeCard(newCard) {
   key = newCard.id;
   var stringifyCard = JSON.stringify(newCard);
   localStorage.setItem(key, stringifyCard);
 }
 
 function restoreCard() {
-  // var retrievedCard = window.localStorage.getItem(key);
   for (var i = 0; i < localStorage.length; i++) {
-  var object = localStorage.getItem(localStorage.key(i))
-  var parsedCard = JSON.parse(object);
-  prependCard(parsedCard);
+    var object = localStorage.getItem(localStorage.key(i))
+    var parsedCard = JSON.parse(object);
+    prependCard(parsedCard);
   }
 }
-
 
 function clearInput() {
   titleInput.value = '';
